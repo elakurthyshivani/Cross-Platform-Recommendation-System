b602c3c34b89844ad1922d2fe16e7a47f324bdc9
elakurthyshivani <95426935+elakurthyshivani@users.noreply.github.com>
2023-01-02 08:19:58 -0500

    Update SendEmail.php
    Added comments and removed my email, password, and path to autoload.php

--- CrossPlatformRecommendationSystem/templates/SendEmail.php
+++ CrossPlatformRecommendationSystem/templates/SendEmail.php
@@ -1,7 +1,11 @@
 <?php
+/** 
+To send an email to the user's email.
+*/
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'C:/xampp/php/vendor/autoload.php';
+# Add the path to the file autoload.php in the following line.
+require 'path-to autoload.php';
 
 
 /*Using Outlook's SMTP because Gmail's SMTP is failing the authentication.
@@ -13,12 +17,15 @@ function sendEmail($toAdr, $subject, $body) {
     $mail->isSMTP(); // Set mailer to use SMTP
     $mail->Host='smtp-mail.outlook.com'; // Specify main SMTP server
     $mail->SMTPAuth=True; // Enable SMTP authentication
-    $mail->Username='crossplatformrs@gmail.com'; // SMTP username
-    $mail->Password='Mi@mi@123$'; // SMTP password
+    
+    # Enter your email and password to send emails to the user.
+    $mail->Username='Enter your email'; // SMTP username
+    $mail->Password='Enter your password'; // SMTP password
     $mail->SMTPSecure='starttls'; // Enable TLS encryption, 'ssl' also accepted
     $mail->Port=587;  
 
-    $mail->setFrom('crossplatformrs@gmail.com', 'Cross Platform Recommendation System'); // Set sender of the mail
+    # Enter your email as from-address to send emails.
+    $mail->setFrom('Enter your email', 'Cross Platform Recommendation System'); // Set sender of the mail
     $mail->addAddress($toAdr); // Add a recipient
        
     $mail->Subject=$subject;
